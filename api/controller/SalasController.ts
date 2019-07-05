9d587a656dd8351786fb6e2616259cc874693a07
Oscar Alfredo Castillejo Salazar <oacs1221@gmail.com>
2019-07-05 08:42:41 -0400

    Salas controller

---
+++ api/controller/SalasController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express';
+import { getManager, InsertResult } from 'typeorm';
+import { Sala, SalaRelations } from '../entities/sala';
+
+export default {
+  async getAll(req: Request, res: Response) {
+    const SalaRepository = getManager().getRepository(Sala);
+    const salas = await SalaRepository.find({
+      relations: [
+        SalaRelations.idEstado,
+        SalaRelations.idPiso,
+        SalaRelations.reservacions
+      ]
+    });
+
+    res.send(salas);
+  },
+  async getById(req: Request, res: Response) {
+    const SalaRepository = getManager().getRepository(Sala);
+    const sala = await SalaRepository.findOne(req.params.id, {
+      relations: [
+        SalaRelations.idEstado,
+        SalaRelations.idPiso,
+        SalaRelations.reservacions
+      ]
+    });
+
+    if (!sala) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    res.send(sala);
+  },
+  async add(req: Request, res: Response) {
+    const SalaRepository = getManager().getRepository(Sala);
+    const sala = req.body;
+    SalaRepository.insert(sala).then((sala$: InsertResult) => {
+      res.send({
+        id: sala$.raw.insertId
+      });
+    });
+
+    if (!Sala) {
+      res.status(404);
+      res.end();
+      return;
+    }
+  },
+  async update(req: Request, res: Response) {
+    const { idEstado, idPiso } = req.body;
+    const { id } = req.params;
+    const SalaRepository = getManager().getRepository(Sala);
+    const sala = await SalaRepository.findOne(id);
+    if (!sala) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    if (idEstado) {
+      sala.idEstado = idEstado;
+    }
+    if (idPiso) {
+      sala.idPiso = idPiso;
+    }
+    SalaRepository.update({ id }, sala);
+    res.send(sala);
+  },
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    const SalaRepository = getManager().getRepository(Sala);
+    const sala = await SalaRepository.findOne(id);
+    if (!sala) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    SalaRepository.delete({ id: sala.id });
+    res.send(sala);
+  }
+};
--- api/entities/sala.ts
+++ api/entities/sala.ts
@@ -1,27 +1,58 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn
+} from 'typeorm';
 import { EstadoSala } from './estado_sala';
 import { Piso } from './piso';
 import { Reservacion } from './reservacion';
+import { TipoSala } from './tipo_sala';
+
+export enum SalaRelations {
+  idPiso = 'idPiso',
+  idEstado = 'idEstado',
+  reservacions = 'reservacions',
+  idTipo = 'idTipo'
+}
 
 @Entity('sala', { schema: 'biblioteca' })
 @Index('fk_salas_pisos_1', ['idPiso'])
 @Index('fk_salas_estados_salas_1', ['idEstado'])
+@Index('fk_salas_tipos_salas_1', ['idTipo'])
 export class Sala {
+  @PrimaryGeneratedColumn({
+    name: 'id'
+  })
+  public id: number;
 
-    @PrimaryGeneratedColumn({
-        name: 'id',
-    })
-    public id: number;
+  @ManyToOne(() => Piso, piso$ => piso$.salas, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_piso' })
+  public idPiso: Piso | null;
 
-    @ManyToOne(() => Piso, (piso$) => piso$.salas, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
-    @JoinColumn({ name: 'id_piso' })
-    public idPiso: Piso | null;
+  @ManyToOne(() => EstadoSala, estadoSala$ => estadoSala$.salas, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_estado' })
+  public idEstado: EstadoSala | null;
 
-    @ManyToOne(() => EstadoSala, (estadoSala$) => estadoSala$.salas, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
-    @JoinColumn({ name: 'id_estado' })
-    public idEstado: EstadoSala | null;
+  @ManyToOne(() => TipoSala, tipoSala$ => tipoSala$.salas, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  @JoinColumn({ name: 'id_tipo' })
+  public idTipo: TipoSala | null;
 
-    @OneToMany(() => Reservacion, (reservacion$) => reservacion$.idSala, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
-    public reservacions: Reservacion[];
-
+  @OneToMany(() => Reservacion, reservacion$ => reservacion$.idSala, {
+    onDelete: 'RESTRICT',
+    onUpdate: 'RESTRICT'
+  })
+  public reservacions: Reservacion[];
 }
